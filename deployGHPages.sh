789e88536d35cfe30821838a8135efeb789068a9
AKS <60446729+AngKS@users.noreply.github.com>
2022-12-21 17:54:22 +0800

    deploy

--- deployGHPages.sh
+++ deployGHPages.sh
@@ -4,11 +4,10 @@ npm run build
 
 echo > .nojekyll
 
-git init
 git checkout -B deployment
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:angks/angks.github.io/V4.git deployment
 
 cd -
